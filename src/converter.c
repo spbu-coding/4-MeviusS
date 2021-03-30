fb2d7c2e279d3b9fc814d760ded2c5568954c972
MeviusS <st090929@student.spbu.ru>
2021-03-30 19:48:01 +0300

    first commit

--- src/converter.c
+++ src/converter.c
@@ -1,188 +1,115 @@
-#include "mybmp.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "qdbmp.h"
+#include "my_realisation.h"
 
-#define PAR_MINE_SIZE 6
-#define PAR_THEIRS_SIZE 8
+#define FIRST_PARAMETER_ERROR 1
+#define SECOND_PARAMETER_ERROR 2
+#define THIRD_PARAMETER_ERROR 3
+#define PARAMETER_COUNT_ERROR 4
+#define NORMAL_PARAMETER_COUNT 4
 
-int write_error(int error_code)
-{
-	switch (error_code)
-	{
-		case INPUT_ERROR:
-		{
-			fprintf(stderr, "Input error!");
-			return -1;
-		}
-		
-		case OUTPUT_ERROR:
-		{
-			fprintf(stderr, "Output error!");
-			return -1;
-		}
-		
-		case OUT_OF_MEMORY_ERROR:
-		{
-			fprintf(stderr, "Out of memory!");
-			return -1;
-		}
-		
-		case OFFSET_ERROR:
-		{
-			fprintf(stderr, "File structure error! (Bit offset too small)");
-			return -2;
-		}
-		
-		case INCORRECT_HEIGHT:
-		{
-			fprintf(stderr, "File structure error! (Incorrect height of image)");
-			return -2;
-		}
-		
-		case INCORRECT_WIDTH:
-		{
-			fprintf(stderr, "File structure error! (Incorrect witdh of image)");
-			return -2;
-		}
-		
-		case NOT_RESERVED:
-		{
-			fprintf(stderr, "File structure error! (Reserved bits not equal to zero)");
-			return -2;
-		}
-		
-		case PLANES_NOT_EQ_1:
-		{
-			fprintf(stderr, "File structure error! (Incorrect number of planes)");
-			return -2;
-		}
-		
-		case INCORRECT_SIGNATURE:
-		{
-			fprintf(stderr, "File is not a BMP!");
-			return -2;
-		}
-		
-		case INCORRECT_BITS_PER_PIXEL:
-		{
-			fprintf(stderr, "Bits per pixel not equal to 8 or 24!");
-			return -2;
-		}
-	}
-	
-	return 0;
+#define error(...) (fprintf(stderr, __VA_ARGS__))
+
+typedef struct {
+    char code_owner[10];
+    char src_file[100];
+    char dst_file[100];
+} PARAMETER_STRUCT;
+
+int read_arguments(int argc, char *argv[], PARAMETER_STRUCT *parameters) {
+    if (argc != NORMAL_PARAMETER_COUNT) {
+        error("Wrong count of parameters");
+        return PARAMETER_COUNT_ERROR;
+    }
+    if (strcmp(argv[1], "--mine") != 0 && strcmp(argv[1], "--theirs") != 0) {
+        error("Wrong first parameter");
+        return FIRST_PARAMETER_ERROR;
+    }
+    strncpy(parameters->code_owner, argv[1], strlen(argv[1]));
+    if (strstr(argv[2], ".bmp") == NULL && strstr(argv[2], ".BMP") == NULL) {
+        error("Wrong second parameter");
+        return SECOND_PARAMETER_ERROR;
+    }
+    strncpy(parameters->src_file, argv[2], strlen(argv[2]));
+    if (strstr(argv[3], ".bmp") == NULL) {
+        error("Wrong third parameter");
+        return THIRD_PARAMETER_ERROR;
+    }
+    strncpy(parameters->dst_file, argv[3], strlen(argv[3]));
+    return 0;
 }
 
-void to_negative_mine(bmp_t *bmp)
-{
-	BYTE r, g, b;
-	
-	if (bmp->bits_per_pixel == 8)
-	{
-		for (unsigned int i = 0; i < 256; i++)
-    	{
-        	get_palette_color(bmp, i, &r, &g, &b);
-        	set_palette_color(bmp, i, ~r, ~g, ~b);
-    	}
-	}
-	else
-	{
-		for (unsigned int i = 0; i < bmp->width; i++)
-			for(unsigned int j = 0; j < bmp->height; j++)
-			{
-				get_pixel(bmp, i, j, &r, &g, &b);
-
-				set_pixel(bmp, i, j, ~r, ~g, ~b);
-			}
-	}
-}
-
-void to_negative_theirs(BMP *bmp)
-{
-	BYTE r, g, b;
-	
-	if (BMP_GetDepth(bmp) == 8)
-	{
-		for (unsigned int i = 0; i < 256; i++)
-    	{
-        	BMP_GetPaletteColor(bmp, i, &r, &g, &b);
-        	BMP_SetPaletteColor(bmp, i, ~r, ~g, ~b);
-    	}
-	}
-	else
-	{
-		unsigned int width = BMP_GetWidth(bmp), height = BMP_GetHeight(bmp);
-		
-		for (unsigned int i = 0; i < width; i++)
-			for (unsigned int j = 0; j < height; j++)
-			{
-				BMP_GetPixelRGB(bmp, i, j, &r, &g, &b);
-				BMP_SetPixelRGB(bmp, i, j, ~r, ~g, ~b);
-			}
-	}
-}
-
-int main(int argc, char **argv)
-{	
-	if (argc != 4 || ((strlen(argv[1]) != PAR_MINE_SIZE || strncmp(argv[1], "--mine", PAR_MINE_SIZE)) && (strlen(argv[1]) != PAR_THEIRS_SIZE || strncmp(argv[1], "--theirs", PAR_THEIRS_SIZE))))
-	{
-		fprintf(stderr, "Incorrect parameters!");
-	
-		return -1;
-	}
-	
-	int error_code = 0;
-	
-	if (strlen(argv[1]) == PAR_MINE_SIZE)
-	{
-		FILE *f = fopen(argv[2], "rb");
-	
-		if (f == NULL)
-			return write_error(INPUT_ERROR);
-
-		bmp_t bmp;
-		init_bmp_t(&bmp);
-		
-		error_code = read_bmp(f, &bmp);
-		
-		if (error_code)
-			return write_error(error_code);
-		
-		fclose(f);
-		
-		bmp.height = abs(bmp.height);
-		to_negative_mine(&bmp);
-		f = fopen(argv[3], "wb");
-	
-		if (f == NULL)
-			return write_error(OUTPUT_ERROR);
-
-		error_code = write_bmp(f, &bmp);
-		
-		if (error_code)
-			return write_error(error_code);
-			
-		free_bmp_t(&bmp);
-	}
-	else
-	{
-		UCHAR	r, g, b;
-		UINT	width, height;
-		UINT	x, y;
-		BMP*	bmp;
-		
-		bmp = BMP_ReadFile(argv[2]);
-		BMP_CHECK_ERROR(stdout, -3);
-
-		width = BMP_GetWidth(bmp);
-		height = BMP_GetHeight(bmp);
-		
-		to_negative_theirs(bmp);
-
-		BMP_WriteFile(bmp, argv[3]);
-		BMP_CHECK_ERROR(stderr, -3);
-
-		BMP_Free(bmp);
-	}
-	
-	return 0;
+int main(int argc, char *argv[]) {
+    PARAMETER_STRUCT parameters;
+    int reading_result = read_arguments(argc, argv, &parameters);
+    if (reading_result != 0) {
+        return reading_result;
+    }
+    char src_path[105];
+    sprintf(src_path, "%s%s", "..\\", parameters.src_file);
+    char dst_path[105];
+    sprintf(dst_path, "%s%s", "..\\", parameters.dst_file);
+    int bits_per_pixel = return_bits_per_pixel(parameters.src_file);
+    if (bits_per_pixel == -1) {
+        error("Cannot open file %s", parameters.src_file);
+        return FILE_OPEN_ERROR;
+    }
+    if (strcmp(parameters.code_owner, "--mine") == 0) {
+        if (bits_per_pixel == 24) {
+            BMP_24 *image = malloc(sizeof(*image));
+            int loading_result = load_bmp_24(image, parameters.src_file);
+            if (loading_result != 0) {
+                free_bmp24(image);
+                return loading_result;
+            }
+            int writing_result = write_negative_bmp_24(image, parameters.dst_file);
+            if (writing_result != 0) {
+                free_bmp24(image);
+                return writing_result;
+            }
+            free_bmp24(image);
+        } else if (bits_per_pixel == 8) {
+            BMP_8 *image = malloc(sizeof(*image));
+            int loading_result = load_bmp_8(image, parameters.src_file);
+            if (loading_result != 0) {
+                free_bmp8(image);
+                return loading_result;
+            }
+            int writing_result = write_negative_bmp_8(image, parameters.dst_file);
+            if (writing_result != 0) {
+                free_bmp8(image);
+                return writing_result;
+            }
+            free_bmp8(image);
+        } else {
+            error("Format is not supported");
+            return -1;
+        }
+    } else {
+        unsigned char r, g, b;
+        BMP *bmp = BMP_ReadFile(parameters.src_file);
+        BMP_CHECK_ERROR(stderr, -1)
+        unsigned long width = BMP_GetWidth(bmp);
+        unsigned long height = BMP_GetWidth(bmp);
+        if (bmp->Header.BitsPerPixel == 24) {
+            for (unsigned int x = 0; x < width; ++x) {
+                for (unsigned int y = 0; y < height; ++y) {
+                    BMP_GetPixelRGB(bmp, x, y, &r, &g, &b);
+                    BMP_SetPixelRGB(bmp, x, y, ~r, ~g, ~b);
+                }
+            }
+        } else if (bmp->Header.BitsPerPixel == 8) {
+            for (long long int i = 0; i < BMP_PALETTE_SIZE_8bpp; i++) {
+                if ((i + 1) % 4 != 0) {
+                    bmp->Palette[i] = ~bmp->Palette[i];
+                }
+            }
+        }
+        BMP_WriteFile(bmp, parameters.dst_file);
+        BMP_CHECK_ERROR(stderr, -1)
+        BMP_Free(bmp);
+    }
+    return 0;
 }
